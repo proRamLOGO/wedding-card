87261e03c621d732aec29013659f5f51ee0e8d8c
proRamLOGO <kapilgupta547@outlook.com>
2021-07-01 13:44:21 +0530

    manif

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/css/style.css',
+       '/bgmusic.mp3'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+   
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+   });
