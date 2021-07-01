4ceebda6489d330ffd0c6b0d280a2044df25078e
proRamLOGO <kapilgupta547@outlook.com>
2021-07-01 13:54:13 +0530

    manif

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,11 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/css/style.css',
-       '/bgmusic.mp3'
-     ]);
-   })
- );
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-   
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-   });
+if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('service-worker.js', {
+     scope: '/wedding-card/' // <--- THIS BIT IS REQUIRED
+ })
+      .then(function(reg){
+        console.log("Yes, it did.");
+     }).catch(function(err) {
+        console.log("No it didn't. This happened:", err)
+    });
+ }
